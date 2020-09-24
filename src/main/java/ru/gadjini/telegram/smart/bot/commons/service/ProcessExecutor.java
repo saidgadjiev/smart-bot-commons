bb6fc5f96afdc5c83f6141ef7ae99186d7f4c9e6
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-09-24 11:51:39 +0300

    ProcessExecutor теперь сервис

--- src/main/java/ru/gadjini/telegram/smart/bot/commons/service/ProcessExecutor.java
+++ src/main/java/ru/gadjini/telegram/smart/bot/commons/service/ProcessExecutor.java
@@ -6,6 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 import ru.gadjini.telegram.smart.bot.commons.exception.ProcessException;
 
 import javax.annotation.PostConstruct;
@@ -15,6 +16,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Arrays;
 
+@Service
 public class ProcessExecutor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ProcessExecutor.class);
