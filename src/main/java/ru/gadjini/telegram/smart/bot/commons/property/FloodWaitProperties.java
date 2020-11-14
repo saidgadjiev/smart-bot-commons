9ab9f771ebf43bee63cab64834d7b9603fe910f6
GadzhievSA <s.gadzhiev@id-mt.ru>
2020-11-14 11:20:14 +0300

    1 min 30 seconds sleep

--- src/main/java/ru/gadjini/telegram/smart/bot/commons/property/FloodWaitProperties.java
+++ src/main/java/ru/gadjini/telegram/smart/bot/commons/property/FloodWaitProperties.java
@@ -3,7 +3,7 @@ package ru.gadjini.telegram.smart.bot.commons.property;
 
 public class FloodWaitProperties {
 
-    public static final int SLEEP_TIME_BEFORE_ATTEMPT = 60000;
+    public static final int SLEEP_TIME_BEFORE_ATTEMPT = 90000;
 
     public static final int FLOOD_WAIT_MAX_ATTEMPTS = 3;
 }
