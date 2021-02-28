72affd2ed2d212b3bdb7582804bd30ebb0767cc2
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-02-28 14:20:46 +0300

    content cleaner для каждого бота свой

--- src/main/java/ru/gadjini/telegram/smart/bot/commons/utils/SmartFileUtils.java
+++ src/main/java/ru/gadjini/telegram/smart/bot/commons/utils/SmartFileUtils.java
@@ -1,11 +1,35 @@
 package ru.gadjini.telegram.smart.bot.commons.utils;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.FileTime;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
 
 public class SmartFileUtils {
 
     private SmartFileUtils() {
 
+    }
+
+    public static boolean isExpired(File file, int days) {
+        try {
+            FileTime creationTime = (FileTime) Files.getAttribute(file.toPath(), "creationTime");
+
+            LocalDateTime creationDateTime = creationTime.toInstant().atZone(ZoneOffset.UTC).toLocalDateTime();
+
+            long between = ChronoUnit.DAYS.between(creationDateTime, LocalDateTime.now());
+
+            if (between > days) {
+                return true;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return false;
     }
 
     public static void mkdirs(File file) {
