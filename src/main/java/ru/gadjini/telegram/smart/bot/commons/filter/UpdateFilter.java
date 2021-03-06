f4d4ecb1f3d6724f8e6166c26616a4c56900c255
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-03-06 10:28:51 +0300

    White list

--- src/main/java/ru/gadjini/telegram/smart/bot/commons/filter/UpdateFilter.java
+++ src/main/java/ru/gadjini/telegram/smart/bot/commons/filter/UpdateFilter.java
@@ -1,15 +1,34 @@
 package ru.gadjini.telegram.smart.bot.commons.filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.gadjini.telegram.smart.bot.commons.model.TgMessage;
+import ru.gadjini.telegram.smart.bot.commons.property.AdminProperties;
 
 @Component
 public class UpdateFilter extends BaseBotFilter {
+
+    private AdminProperties adminProperties;
+
+    @Autowired
+    public UpdateFilter(AdminProperties adminProperties) {
+        this.adminProperties = adminProperties;
+    }
 
     @Override
     public void doFilter(Update update) {
         if (update.hasMessage() && update.getMessage().getChat().isUserChat()
                 || update.hasCallbackQuery() && update.getCallbackQuery().getMessage().getChat().isUserChat()) {
+            if (!CollectionUtils.isEmpty(adminProperties.getWhiteList())) {
+                long chatId = TgMessage.getChatId(update);
+
+                if (!adminProperties.getWhiteList().contains(chatId)) {
+                    return;
+                }
+            }
+
             super.doFilter(update);
         }
     }
