7b9e24460488ebedaf274f9d97a82f1d7131405d
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-03-28 19:42:26 +0300

    Вывод id коммита сборки

--- src/main/java/ru/gadjini/telegram/smart/bot/commons/controller/BotController.java
+++ src/main/java/ru/gadjini/telegram/smart/bot/commons/controller/BotController.java
@@ -1,9 +1,11 @@
 package ru.gadjini.telegram.smart.bot.commons.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.gadjini.telegram.smart.bot.commons.model.BotHealth;
 
 import javax.ws.rs.core.MediaType;
 
@@ -11,8 +13,11 @@ import javax.ws.rs.core.MediaType;
 @RequestMapping("/bot")
 public class BotController {
 
-    @GetMapping(value = "/health", produces = MediaType.TEXT_PLAIN)
+    @Value("${git.commit.id}")
+    private String commitId;
+
+    @GetMapping(value = "/health", produces = MediaType.APPLICATION_JSON)
     public ResponseEntity<?> hello() {
-        return ResponseEntity.ok("I'm alive");
+        return ResponseEntity.ok(new BotHealth("I'm alive", commitId));
     }
 }
