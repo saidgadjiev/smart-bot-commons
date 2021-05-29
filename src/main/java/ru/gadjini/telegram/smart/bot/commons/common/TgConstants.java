1e339421cd3d9936d99f00f11f25fdb758301b52
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-05-29 18:38:43 +0300

    Video note max size

--- src/main/java/ru/gadjini/telegram/smart/bot/commons/common/TgConstants.java
+++ src/main/java/ru/gadjini/telegram/smart/bot/commons/common/TgConstants.java
@@ -9,6 +9,8 @@ public class TgConstants {
     //2000 MB
     public static final long LARGE_FILE_SIZE = 2000 * 1024 * 1024;
 
+    public static final long VIDEO_NOTE_MAX_FILE_SIZE = 8 * 1024 * 1024;
+
     public static final int PAYMENTS_AMOUNT_FACTOR = 100;
 
     public static final int TEXT_LENGTH_LIMIT = 4000;
