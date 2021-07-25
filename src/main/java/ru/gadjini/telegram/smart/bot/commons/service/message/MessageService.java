996fc6da5435f954637ad3365e9301ee874cc103
GadzhievSA <s.gadzhiev@id-mt.ru>
2021-07-25 21:10:58 +0300

    Добавил анализ изменилось ли сообщение или нет

--- src/main/java/ru/gadjini/telegram/smart/bot/commons/service/message/MessageService.java
+++ src/main/java/ru/gadjini/telegram/smart/bot/commons/service/message/MessageService.java
@@ -8,8 +8,11 @@ import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageCa
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import ru.gadjini.telegram.smart.bot.commons.utils.TextUtils;
 
 import java.util.Locale;
+import java.util.Objects;
 import java.util.function.Consumer;
 
 public interface MessageService {
@@ -28,11 +31,35 @@ public interface MessageService {
 
     void editMessage(EditMessageText messageContext, boolean ignoreException);
 
+    default void editMessage(String text, InlineKeyboardMarkup inlineKeyboardMarkup,
+                             EditMessageText messageContext, boolean ignoreException) {
+        if (Objects.equals(text, TextUtils.removeHtmlTags(messageContext.getText()))
+                && Objects.equals(inlineKeyboardMarkup, messageContext.getReplyMarkup())) {
+            return;
+        }
+        editMessage(messageContext, ignoreException);
+    }
+
     default void editMessage(EditMessageText messageContext) {
         editMessage(messageContext, true);
     }
 
+    default void editMessage(String text, InlineKeyboardMarkup inlineKeyboardMarkup, EditMessageText messageContext) {
+        if (Objects.equals(text, TextUtils.removeHtmlTags(messageContext.getText()))
+                && Objects.equals(inlineKeyboardMarkup, messageContext.getReplyMarkup())) {
+            return;
+        }
+        editMessage(messageContext, true);
+    }
+
     void editKeyboard(EditMessageReplyMarkup editMessageReplyMarkup, boolean ignoreException);
+
+    default void editKeyboard(InlineKeyboardMarkup currentKeyboard, EditMessageReplyMarkup editMessageReplyMarkup, boolean ignoreException) {
+        if (Objects.equals(currentKeyboard, editMessageReplyMarkup.getReplyMarkup())) {
+            return;
+        }
+        editKeyboard(editMessageReplyMarkup, ignoreException);
+    }
 
     void editMessageCaption(EditMessageCaption context);
 
